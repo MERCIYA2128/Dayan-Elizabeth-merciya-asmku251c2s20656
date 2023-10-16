80288fdcdb4af03708746f57caab4077fe5973f7
MERCIYA2128 <148033299+MERCIYA2128@users.noreply.github.com>
2023-10-16 13:30:17 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,54 @@
+... implement a class called bank account that represents a bank account the class should have private
+attributes for account number account holder name, and account balance including method to
+deposit money withdraw money, and display the account balance
+cannot be accepted directly from outside the class write a program to create an instance of the
+Bank account class and test the deposit and with drawl functionality."
+
+
+class background;
+
+def__intt__(self, account _number, account_holder_name, initial_balance=0.0)
+  self.__account_number=account_number
+self.__account_holder_name=account_holder_name
+self.__account_blance=initial_blance
+
+def deposit(self, amount):
+  if Amount>0:
+    self.__account_balance+=amount
+    self.__account_balance=self_account balance+amount
+  else:
+    print("invalid deposit amount")
+    print("deposited €{}.new balance:€{}*. format (account,self __account_balance))
+    
+else:
+  print("invalid deposit amount.please a positive amount.")
+  
+def withdraw(self ammount):
+if amount>0and amount<=self.__account_balance:
+self.__account_balance-=amount
+print("withdraw €{}.new balance:€{}*. format (account, self__account_balance))
+self.__account_balance-=amount 
+else:
+print("invalid withdraw) amount or insufficient balance.")
+
+
+
+def display_balance(self):
+  print("account balance for{}(account #{})". format {
+    self.__account_holder_name,self.__account_number
+self.__account_balanace))
+
+
+    creat balance of bank account
+account=bank account (account _number"123456789",
+                     account_holder_name."hari prabu
+initial_balance=5000.0))
+
+account.disposit(5000.0)
+account.dispaly_balance()
+account.diposit(500.0)
+account.withdraw(200.0)
+#account.display=balance 
+
+  "
+
